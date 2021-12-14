22c21ea5a475c71aa7815c70f9b2967578f38e0c
Anshika <85496946+groot1302@users.noreply.github.com>
2021-12-14 20:21:16 +0530

    Add files via upload

---
+++ SizeCheck.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+cout<<"ANSHIKA MEHTA, Section-G, Univ.Roll.no-2018188, Class Roll.no-19"<<endl;
+cout<<"-----Size of given data types are-------"<<endl;
+cout<<"Size of int is: "<<sizeof(int)<<endl;
+cout<<"Size of unsigned int is: "<<sizeof(unsigned int)<<endl;
+cout<<"Size of signed int is: "<<sizeof(signed int)<<endl;
+cout<<"Size of char is: "<<sizeof(char)<<endl;
+cout<<"Size of signed char is: "<<sizeof(signed char)<<endl;
+cout<<"Size of unsigned char is: "<<sizeof(unsigned char)<<endl;
+cout<<"Size of float is: "<<sizeof(float)<<endl;
+cout<<"Size of double is: "<<sizeof(double)<<endl;
+
+return 0;
+}
